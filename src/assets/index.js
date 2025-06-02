19b86866553a31f10a6d5c288e7441d0e9b391bb
Diamond <dbrown@pursuit.org>
2025-06-02 17:38:09 -0400

    edited components

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -13,25 +12,13 @@ import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-// import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-// import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-// import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 
 export {
-  logo,
   backend,
   creator,
   mobile,
@@ -45,18 +32,9 @@ export {
   git,
   html,
   javascript,
-  // mongodb,
   nodejs,
   reactjs,
-  // redux,
   tailwind,
-  // typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
+  
 };
